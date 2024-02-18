e8c4782e46fe2311a7c5983a27d38c59d541532f
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-18 09:47:06 -0700

    Add instance variables for product count and category count in Admin dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  
   def show
+    @product_count = Product.sum(:quantity)
+    @category_count = Category.count
   end
 end
