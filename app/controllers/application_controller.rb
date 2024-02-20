737bbb7f18e1f0e00f31e068e2518095fa5bce9e
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-20 11:10:29 -0700

    Add a comment for reference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,7 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  #Below methods are used to look up the user, if they're logged in, and save their user object to a variable called @current_user. The helper_method line below current_user allows us to use @current_user in our view files. Authorize is for sending someone to the login page if they aren't logged in - this is how we keep certain pages our site secure... user's have to login before seeing them
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
