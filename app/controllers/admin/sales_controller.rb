9ff780998a7c0fa4d674d0c6f83686ae5c71e494
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-23 21:13:54 -0700

    Add sale model

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SalesController < ApplicationController
 
   def index
-    # if no render statement is present then render :index is implied
+  #  if no render statement is present then render :index is implied
+    @sales = Sale.all
   end
 end
