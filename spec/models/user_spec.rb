ff4368084670eec5492457e9a68805af62d3c603
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 11:55:52 -0700

    Add a test to validate a new user is successfully created with all valid fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'successfully creates a new user when all valid fields are entered' do
+    @user = User.new(
+      name: 'Hisban',
+      email: 'test@test.com',
+      password: '12345',
+      password_confirmation: '12345'
+    )
+    expect(@user).to be_valid
+  end
 end
