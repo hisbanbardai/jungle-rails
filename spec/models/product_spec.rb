f32b0e9d98d66ec10c9e4ccdf3c0608fc5d53f77
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 10:51:02 -0700

    Add a test to validate a product is not valid without a price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,7 +12,14 @@ RSpec.describe Product, type: :model do
       @category = Category.new
       @product = Product.new(price: 100, quantity: 10, category: @category)
       expect(@product).to_not be_valid
-      expect(@product.errors.full_messages).to eq ["Name can't be blank"]
+      expect(@product.errors.full_messages[0]).to eq "Name can't be blank"
+    end
+
+    it 'is not valid without a price' do
+      @category = Category.new
+      @product = Product.new(name: 'Test product', quantity: 10, category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages[2]).to eq "Price can't be blank"
     end
   end
 end
