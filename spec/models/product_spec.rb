45848dbbcfda723d701f5e368c8bce82d842982a
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 11:13:57 -0700

    Set validation fields to nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,27 +10,27 @@ RSpec.describe Product, type: :model do
 
     it 'is not valid without a name' do
       @category = Category.new
-      @product = Product.new(price: 100, quantity: 10, category: @category)
+      @product = Product.new(name: nil, price: 100, quantity: 10, category: @category)
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages[0]).to eq "Name can't be blank"
     end
 
     it 'is not valid without a price' do
       @category = Category.new
-      @product = Product.new(name: 'Test product', quantity: 10, category: @category)
+      @product = Product.new(name: 'Test product', price_cents: nil, quantity: 10, category: @category)
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages[2]).to eq "Price can't be blank"
     end
 
     it 'is not valid without a quantity' do
       @category = Category.new
-      @product = Product.new(name: 'Test product', price: 100, category: @category)
+      @product = Product.new(name: 'Test product', price: 100, quantity: nil, category: @category)
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages[0]).to eq "Quantity can't be blank"
     end
 
     it 'is not valid without a category' do
-      @product = Product.new(name: 'Test product', price: 100, quantity: 10)
+      @product = Product.new(name: 'Test product', price: 100, quantity: 10, category:nil)
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages[1]).to eq "Category can't be blank"
     end
