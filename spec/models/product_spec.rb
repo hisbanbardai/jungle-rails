c1126f553a60582d60d6d7a57a630916672e77e0
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 10:41:18 -0700

    Add an initial test to check if a product with valid attributes gets saved successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe 'Validations' do
+    it 'is valid with valid attributes' do
+      @category = Category.new
+      @product = Product.new(name: 'Test product', price: 100, quantity: 10, category: @category)
+      expect(@product).to be_valid
+    end
+  end
 end
