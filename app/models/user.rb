c5d26d1d12bb0bce867de457fd24ff0f0224342f
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 12:32:59 -0700

    Add presence and uniqueness validations for email in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
