a233e039547da38a1891d8516d92fe1402ee11da
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 12:39:57 -0700

    Add presence validation for name in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
