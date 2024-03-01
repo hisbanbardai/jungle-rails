d738bc0b68bf1fa675616674532138ffe1291c36
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-01 12:17:09 -0700

    Add presence validation for password_confirmation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 #This is the line of code that gives our User model authentication methods via bcrypt.  
   has_secure_password
+
+  validates :password_confirmation, presence: true
 end
