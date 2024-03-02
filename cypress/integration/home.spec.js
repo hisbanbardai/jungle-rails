0c2c1148c23eddcaec8db33b5405058456007e36
Hisban Shiraz <hisbanbardai@gmail.com>
2024-03-02 07:25:05 -0700

    Add a test to validate there is total 2 products on the page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,17 @@
 describe("Home page", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
+  });
+
+  it("Should show the home page", () => {
+    cy.get(".title h1").should("have.text", "The Jungle");
   });
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 });
